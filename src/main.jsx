fa50506f9c8af1784f0a3f87a343f65fefe9cdaf
Yaretzi Ortiz <ortizyarezti@gmail.com>
2026-06-25 12:27:15 -0600

    Cambios en main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
+// original
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root')
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
